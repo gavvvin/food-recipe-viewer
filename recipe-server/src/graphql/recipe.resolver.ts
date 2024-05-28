17e6c4ac2015f9f8ec7852af0c3378c476232858
Gavin Sin <Gavin.Sin@ibm.com>
2024-05-28 12:51:32 +0100

    modify file structure

--- recipe-server/src/graphql/recipe.resolver.ts
+++ recipe-server/src/graphql/recipe.resolver.ts
@@ -1,24 +1,8 @@
-// import { RecipeMockData } from "../datasource/recipeMockData";
 import { getRecipes } from "../db/connection";
 
 const recipeResolver = {
   Query: {
     recipes: async () => await getRecipes(),
-    // getChargePointsByDistance: (
-    //   __root: any,
-    //   { orderBy }: { orderBy: String },
-    // ) => {
-    //   if (orderBy === "asc")
-    //     ChargePointDataMock.sort(
-    //       (a, b) => parseInt(a.distance) - parseInt(b.distance),
-    //     );
-    //   if (orderBy === "desc")
-    //     ChargePointDataMock.sort(
-    //       (a, b) => parseInt(b.distance) - parseInt(a.distance),
-    //     );
-
-    //   return ChargePointDataMock;
-    // },
   },
 };
 
